fce878071870c7636ae6b43e5abd89d9af922792
sravanithummapudi <tlsravani2003@gmail.com>
2022-10-02 02:09:30 +0530

    Intial commit

--- src/Components/Caretaker/subcontent/ImmediateActionMsg.jsx
+++ src/Components/Caretaker/subcontent/ImmediateActionMsg.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 
 function ImmediateActionMsg() {
+ let token = "Bearer " + localStorage.getItem("token");
+  async function CallApi(){
+  const response = await fetch(process.env.REACT_APP_BASE_URL+"caretaker/getMessages", {
+    method: "POST",
+    headers: { 
+      "Authorization" : token,
+      "Content-Type": "application/json" },
+    body: JSON.stringify({id: localStorage.getItem("id")}),
+  });
+  const content = await response.json();
+  console.log(content.action.message[0]);
+}
+
+useEffect(() => {
+  CallApi();  
+})
   return (
     <div>ImmediateActionMsg</div>
+    
+
   )
 }
 
