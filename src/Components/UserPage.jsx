2c7b26a1c3f1103619963496e0ab543baa1405a4
anushkapatil18 <anushkabpatil11@gmail.com>
2022-10-01 23:00:57 +0530

    bg

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -138,8 +138,11 @@ function UserPage() {
         </div>*/}
   </nav>
   <div style={{ 
-      backgroundImage: `url("https://d2evkimvhatqav.cloudfront.net/images/_960xAUTO_crop_center-center_none/mother_cow_calf_31883069.jpg?v=1650463658,0.6021,0.4284")` 
-    }}className="py-8 px-80 bg-slate-100 text-white text-xl">
+      backgroundImage: `url("https://d2evkimvhatqav.cloudfront.net/images/_960xAUTO_crop_center-center_none/mother_cow_calf_31883069.jpg?v=1650463658,0.6021,0.4284")` ,
+    height:"100%",
+	backgroundPosition: "center",
+	backgroundRepeat: "no-repeat",
+	backgroundSize: "cover"}}className="py-8 px-80 bg-slate-100 text-white text-xl">
 		<marquee class="marq"
 				direction="down"
 				behavior="scroll"
