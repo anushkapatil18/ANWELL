2fe33619facbd5bbe78effd1da68403c1a51d304
sravanithummapudi <tlsravani2003@gmail.com>
2022-10-02 02:36:06 +0530

    Initial commit

--- src/Components/Caretaker/subcontent/ImmediateActionMsg.jsx
+++ src/Components/Caretaker/subcontent/ImmediateActionMsg.jsx
@@ -1,25 +1,38 @@
-import React, { useEffect, useLayoutEffect } from 'react'
+
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 
 function ImmediateActionMsg() {
  let token = "Bearer " + localStorage.getItem("token");
+ const[t,setT]=useState(false);
+ const[msg,setMsg]  = useState("");
+ const handleChange=()=>{
+  setT("This is message for care taker bhavik ");
+ }
   async function CallApi(){
   const response = await fetch(process.env.REACT_APP_BASE_URL+"caretaker/getMessages", {
     method: "POST",
     headers: { 
       "Authorization" : token,
+
       "Content-Type": "application/json" },
     body: JSON.stringify({id: localStorage.getItem("id")}),
   });
   const content = await response.json();
   console.log(content.action.message[0]);
+  setMsg(content.action.message[0]);
+  setT(true);
+  
 }
 
 useEffect(() => {
   CallApi();  
 })
   return (
-    <div>ImmediateActionMsg</div>
-    
+   <>
+    {
+     t  ? (<>{msg}</>) : "NO MESSAGE"
+    }
+    </>
 
   )
 }
